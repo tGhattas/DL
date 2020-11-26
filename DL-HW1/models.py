fc90e7bb0f72bc18281deec34151a812d95ee254
w552295 <tamer.ghattas@cs.huji.ac.il>
2020-11-26 13:05:58 +0200

    model refac

--- DL-HW1/models.py
+++ DL-HW1/models.py
@@ -3,21 +3,21 @@ from tensorflow.keras import Model, Sequential
 import hyper_params
 
 
-# class MLP(Model):
-#
-#   def __init__(self, input_shape=(hyper_params.INPUT_DIM,), output_shape=hyper_params.OUTPUT_DIM):
-#     super(MLP, self).__init__()
-#
-#     # self.flatten = Flatten()
-#     self.d1 = Dense(350, input_shape=input_shape, activation='relu')
-#     self.d2 = Dense(50, activation='relu')
-#     self.d3 = Dense(output_shape, activation='softmax')
-#
-#   def call(self, x):
-#     x = self.d1(x)
-#     x = self.d2(x)
-#     x = self.d3(x)
-#     return x
+class MLP(Model):
+
+  def __init__(self, input_shape=(hyper_params.INPUT_DIM,), output_shape=hyper_params.OUTPUT_DIM):
+    super(MLP, self).__init__()
+
+    # self.flatten = Flatten()
+    self.d1 = Dense(350, input_shape=input_shape, activation='relu')
+    self.d2 = Dense(50, activation='relu')
+    self.d3 = Dense(output_shape, activation='softmax')
+
+  def call(self, x):
+    x = self.d1(x)
+    x = self.d2(x)
+    x = self.d3(x)
+    return x
 
 
 MLP_s = Sequential(name='MLP_s', layers=[
